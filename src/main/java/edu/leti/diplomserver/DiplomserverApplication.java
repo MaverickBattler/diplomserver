2693077b52db3a22e9b2d70e086273ea38eab325
Kirill Mokhnatkin <librarian4832@gmail.com>
2022-03-10 04:20:17 +0300

    Added support for war packaging

--- src/main/java/edu/leti/diplomserver/DiplomserverApplication.java
+++ src/main/java/edu/leti/diplomserver/DiplomserverApplication.java
@@ -2,9 +2,17 @@ package edu.leti.diplomserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DiplomserverApplication {
+public class DiplomserverApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DiplomserverApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(DiplomserverApplication.class, args);
     }
