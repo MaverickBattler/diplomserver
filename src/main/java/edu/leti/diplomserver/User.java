2b09b4e9830177105f1e304c8f7d7a37609c8351
Kirill Mokhnatkin <librarian4832@gmail.com>
2022-03-24 18:32:29 +0300

    Added User class with basic fields

---
+++ src/main/java/edu/leti/diplomserver/User.java
@@ -0,0 +1,10 @@
+package edu.leti.diplomserver;
+
+public class User {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String fatherName;
+    private String email;
+    private String password;
+}
